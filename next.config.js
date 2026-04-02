1d1d8157102e36f23b599aa7b7e76843ce8f1793
AmPrinceInKingdom <156967852+AmPrinceInKingdom@users.noreply.github.com>
2026-04-02 23:46:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -17,10 +16,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "*",
+        hostname: "**",
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
